81f02b6f95df93b787a02fc17c2e6f4f49dc9019
Yannis699 <yannis.cristini@gmail.com>
2022-07-21 16:14:23 +0200

    Mise en place du formulaire inscription utilisateur

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class LoginController extends AbstractController
 {
     #[Route('/signup', name: 'signup')]
-    public function signup(): Response
+    public function signup(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): Response
     {
-        return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+        $user = new User();
+        $userForm = $this->createForm(UserType::class, $user);
+        $userForm->handleRequest($request);
+            if($userForm->isSubmitted() && $userForm->isValid()) {
+                $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+                $em->persist($user);
+                $em->flush();
+                $this->addFlash('success', 'Bienvenue !');
+                return $this->redirectToRoute('login');
+            }
+
+        return $this->render('login/signup.html.twig', [
+            'sign_up_form' => $userForm->createView(),
         ]);
     }
 
@@ -25,7 +41,7 @@ class LoginController extends AbstractController
     }
 
     #[Route('/logout', name: 'logout')]
-    public function logout(): Response
+    public function logout()
     {
         return $this->render('login/index.html.twig', [
             'controller_name' => 'LoginController',
